a3ec37766da16e3f99a1f6b599a1d8b1a3440a34
L200180141 <44077866+L200180141@users.noreply.github.com>
2018-11-29 20:30:48 +0700

    Add files via upload

---
+++ algopro_d)_141_praktikumke9.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python menulis data ke dalam berkas
+nama = aji mustaqim
+nim = l200180141
+kegiatan 1
+##berkas = open("l200180141.txt", "w")
+##berkas.write("NIM saya l200180141.""\n ")
+##berkas.write("Ttl saya 18-10-2000.""\n ")
+##berkas.write("Nama lengkap saya Aji Mustaqim. ")
+##berkas.close()
+kegiatan2
+##!/usr/bin/python menulis data ke dalam berkas
+##berkas = open("l200180141", "w")
+##berkas.write("NIM saya l200180141. ")
+##berkas.write("Ttl saya 10-18-2000. ")
+##berkas.write("Nama lengkap saya Aji Mustaqim. ")
+##berkas.write("kota kelahiran karanganyar. ")
+##berkas.close()
+##
+##berkas = open("l200180141", "r")
+##print (berkas.readlines())
+##berkas.close()
+##
+
+
+kegiatan 3
+##import shelve
+##a=open('l200180141', 'r')
+##nim=a.readline()
+##tl=a.readline()
+##nama=a.readline
+##a.close()
+##
+##a=shelve.open('aji')
+##a['bb]=[nim, tl, nama]
+##a.close
+kegiatan 4  
+##import shelve
+##a=shelve.open('aji')
+##print 'nim:', a['b'][0]
+##print 'tl:', a['b'][1]
+##print 'nama:', a['b'][2]
+##a.close()
